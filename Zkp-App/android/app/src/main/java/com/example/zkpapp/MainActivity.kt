5ff6194242c67fef1acfd043b8e4f773c543f025
Hamid0004 <165746548+Hamid0004@users.noreply.github.com>
2026-01-01 11:30:33 +0000

    Day 33: Added Interactive Button Feature

--- Zkp-App/android/app/src/main/java/com/example/zkpapp/MainActivity.kt
+++ Zkp-App/android/app/src/main/java/com/example/zkpapp/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.zkpapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Screen se cheezein dhoondo
+        val textView = findViewById<TextView>(R.id.myTextView)
+        val button = findViewById<Button>(R.id.myButton)
+
+        // Jab Button dabaya jaye
+        button.setOnClickListener {
+            textView.text = "You Clicked Me! 🎉"
+            Toast.makeText(this, "Success!", Toast.LENGTH_SHORT).show()
+        }
     }
 }
